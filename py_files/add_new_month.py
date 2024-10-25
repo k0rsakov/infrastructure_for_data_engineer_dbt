f665f636915d76a7f2c05ebf347c29476d903c80
k0rsakov <korsakov.iyu@gmail.com>
2024-10-25 14:54:22 +0300

    Изменен проект infrastructure_for_data_engineer_dbt: - Небольшие улучшения

--- py_files/add_new_month.py
+++ py_files/add_new_month.py
@@ -11,31 +11,13 @@ pg = PostgreSQL(
 
 fake = Faker(locale="ru_RU")
 
-pg.execute_script(
-    """
-    DROP TABLE IF EXISTS users;
-
-    CREATE TABLE IF NOT EXISTS users
-    (
-        id uuid PRIMARY KEY,
-        created_at timestamptz DEFAULT now(),
-        first_name varchar,
-        last_name varchar,
-        middle_name varchar,
-        birthday date,
-        email varchar,
-        city varchar
-    );
-    """,
-)
-
 list_of_dict = []
 for _ in range(3000):
     dict_ = {
         "id": uuid.uuid4(),
         "created_at": fake.date_time_ad(
             start_datetime=datetime.date(year=2025, month=1, day=1),
-            end_datetime=datetime.date(year=2025, month=1, day=31),
+            end_datetime=datetime.date(year=2025, month=2, day=1),
         ),
         "first_name": fake.first_name(),
         "last_name": fake.last_name(),
