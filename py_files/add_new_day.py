254ea08588e84df791ed66ceb79d53d811163ab4
k0rsakov <korsakov.iyu@gmail.com>
2024-10-25 13:45:01 +0300

    Изменен проект infrastructure_for_data_engineer_dbt: - Добавлен файл с новыми пользователями

---
+++ py_files/add_new_day.py
@@ -0,0 +1,58 @@
+import datetime
+import uuid
+
+import pandas as pd
+from connectors_to_databases import PostgreSQL
+from faker import Faker
+
+pg = PostgreSQL(
+    database="dbt",
+)
+
+fake = Faker(locale="ru_RU")
+
+pg.execute_script(
+    """
+    DROP TABLE IF EXISTS users;
+
+    CREATE TABLE IF NOT EXISTS users
+    (
+        id uuid PRIMARY KEY,
+        created_at timestamptz DEFAULT now(),
+        first_name varchar,
+        last_name varchar,
+        middle_name varchar,
+        birthday date,
+        email varchar,
+        city varchar
+    );
+    """,
+)
+
+list_of_dict = []
+for _ in range(3000):
+    dict_ = {
+        "id": uuid.uuid4(),
+        "created_at": fake.date_time_ad(
+            start_datetime=datetime.date(year=2025, month=1, day=1),
+            end_datetime=datetime.date(year=2025, month=1, day=31),
+        ),
+        "first_name": fake.first_name(),
+        "last_name": fake.last_name(),
+        "middle_name": fake.middle_name(),
+        "birthday": fake.date_time_ad(
+            start_datetime=datetime.date(year=1980, month=1, day=1),
+            end_datetime=datetime.date(year=2005, month=12, day=31),
+        ),
+        "email": fake.email(),
+        "city": fake.city(),
+    }
+
+    list_of_dict.append(dict_)
+
+df = pd.DataFrame(data=list_of_dict)
+
+pg.insert_df(
+    df=df,
+    table_name="users",
+)
