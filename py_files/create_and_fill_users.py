e7c5e254c78848574fffd1fa3b9db98806a98581
k0rsakov <korsakov.iyu@gmail.com>
2024-10-26 05:32:23 +0300

    Изменен проект infrastructure_for_data_engineer_dbt: - Добавлен пример работы с source

--- py_files/create_and_fill_users.py
+++ py_files/create_and_fill_users.py
@@ -35,14 +35,14 @@ for _ in range(1000000):
         "id": uuid.uuid4(),
         "created_at": fake.date_time_ad(
             start_datetime=datetime.date(year=2024, month=1, day=1),
-            end_datetime=datetime.date(year=2024, month=12, day=31),
+            end_datetime=datetime.date(year=2025, month=1, day=1),
         ),
         "first_name": fake.first_name(),
         "last_name": fake.last_name(),
         "middle_name": fake.middle_name(),
         "birthday": fake.date_time_ad(
             start_datetime=datetime.date(year=1980, month=1, day=1),
-            end_datetime=datetime.date(year=2005, month=12, day=31),
+            end_datetime=datetime.date(year=2005, month=1, day=1),
         ),
         "email": fake.email(),
         "city": fake.city(),
